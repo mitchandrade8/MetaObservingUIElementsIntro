512bdaa52cd5b7820a47e65b1b4d7f45d2ce6908
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-01 05:57:55 -0600

    Initial Commit

---
+++ MetaObservingUIElementsIntro/MetaObservingUIElementsIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaObservingUIElementsIntroApp.swift
+//  MetaObservingUIElementsIntro
+//
+//  Created by Mitch Andrade on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MetaObservingUIElementsIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
