fb2dd5ba02aa73f87ec3f55ea1dc211be06ba8f5
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-01 22:20:28 -0600

    -- Meta overview of observing changes and where this applies in SwiftUI

---
+++ MetaObservingUIElementsIntro/Notes.swift
@@ -0,0 +1,9 @@
+//
+//  Notes.swift
+//  MetaObservingUIElementsIntro
+//
+//  Created by Mitch Andrade on 9/1/23.
+//
+
+import Foundation
+
---
+++ MetaObservingUIElementsIntro/ObservingChangesExample.swift
@@ -0,0 +1,23 @@
+//
+//  ObservingChangesExample.swift
+//  MetaObservingUIElementsIntro
+//
+//  Created by Mitch Andrade on 9/1/23.
+//
+
+import SwiftUI
+
+struct ObservingChangesExample: View {
+    
+    @State private var inputValue: Double = 0
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ObservingChangesExample_Previews: PreviewProvider {
+    static var previews: some View {
+        ObservingChangesExample()
+    }
+}
