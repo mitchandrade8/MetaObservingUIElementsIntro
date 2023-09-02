1c904493b7f9d6bcd0ab7fe93dad958dcf574df4
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-02 07:41:22 -0600

    -- Taking notes on property wrappers in SwiftUI

--- MetaObservingUIElementsIntro/Notes.swift
+++ MetaObservingUIElementsIntro/Notes.swift
@@ -7,3 +7,16 @@
 
 import Foundation
 
+// Elements on the screen need to be updated reguraly
+
+//  Updates can be calculation or user input
+
+// MARK: - @State
+
+// State is a property wrapper
+
+// State is only good when working within 1 view
+
+// MARK: - Passing information to another view
+
+
